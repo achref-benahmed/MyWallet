efb03c85ddd39dbcc75ccbf9f3cc65573d714419
achrefab <achref.benhmed@gmail.com>
2016-11-06 16:40:50 +0100

    user role

--- server/boot/script.js
+++ server/boot/script.js
@@ -13,17 +13,8 @@ User.create([
     Role.create({
       name: 'admin'
     }, function(err, role) {
-      if (err) return debug(err);
-      debug(role);
- 
-      // Make Bob an admin
-      role.principals.create({
-        principalType: RoleMapping.USER,
-        principalId: users[2].id
-      }, function(err, principal) {
-        if (err) return debug(err);
-        debug(principal);
-      });
-    });
+      if (err) throw err;
+      console.log('Created role:', role);
+       });
   });
 };
